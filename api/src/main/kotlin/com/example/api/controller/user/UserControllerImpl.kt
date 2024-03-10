69c5e8f32bbb75dea58bc5ce9fa649d4bc4503fa
s_yamaga <s_yamaga@codmon.co.jp>
2024-03-10 13:24:27 +0900

    routes react pages

--- api/src/main/kotlin/com/example/api/controller/user/UserControllerImpl.kt
+++ api/src/main/kotlin/com/example/api/controller/user/UserControllerImpl.kt
@@ -4,8 +4,10 @@ import com.example.api.controller.gen.User
 import com.example.api.controller.gen.UserApi
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
+@RestController
 class UserControllerImpl() : UserApi {
     override fun getUsers(): ResponseEntity<List<User>> {
         return ResponseEntity(
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import {FC} from "react";
+import {Users} from "./pages/users/Users.tsx";
+import {ErrorPage} from "./pages/ErrorPage.tsx";
+import {Root} from "./pages/Root.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "users",
+                element: <Users />,
+            },
+        ],
+    },
+]);
+
+export const Router: FC = () => {
+    return <RouterProvider router={router} />;
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <Router />
+        </div>
+    )
 }
 
 export default App
